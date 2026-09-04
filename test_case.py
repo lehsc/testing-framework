13053a385b3eecacf699a95f92cd599f27004872
lehsc <lehcaldeira36@gmail.com>
2026-09-04 11:34:50 -0300

    7. Comandos Assert

--- test_case.py
+++ test_case.py
@@ -13,10 +13,30 @@ class TestCase:
             result.add_failure(self.test_method_name)
         except Exception as e:
             result.add_error(self.test_method_name)
-        self.tear_down()
+        self.tear_down()    
 
     def set_up(self):
         pass
 
     def tear_down(self):
         pass
+
+    def assert_equal(self, first, second):
+            if first != second:
+                msg = f'{first} != {second}'
+                raise AssertionError(msg)
+
+    def assert_true(self, expr):
+        if not expr:
+            msg = f'{expr} is not true'
+            raise AssertionError(msg)
+
+    def assert_false(self, expr):
+        if expr:
+            msg = f'{expr} is not false'
+            raise AssertionError(msg)
+
+    def assert_in(self, member, container):
+        if member not in container:
+            msg = f'{member} not found in {container}'
+            raise AssertionError(msg)
